8200ec2518a86daa9d17ee345fcbddda833f0d14
Cristian <cw14@inf.ufpr.br>
2015-09-20 23:25:11 -0300

    Create a basic parity system
    Signed-off-by: Cristian <cw14@inf.ufpr.br>

--- utils.c
+++ utils.c
@@ -5,6 +5,32 @@ int print_message(Message *m) {
     return 1;
 }
 
+char get_parity(Message *m) {
+    /* This function will get a message and create a byte of parity of it. It will work in this way:
+    I will store the xor from all LSB bits in v[7], the next in v[6], and so on, until the xor of the MSB bits
+    is stored in v[1]. After that, I will dislocate them and create only one bit with the correct value.
+    */
+    int count,revcount,i,v[8] = [0,0,0,0,0,0,0,0], mask;
+    int len = m->attr.len; // Just for saving words inside the code.
+    char res = 0; // Do we need to use a cast?
+    // Ill start with the MSB bits.
+    for(count = 0,revcount = 7; count < 8; count++,revcount--) {
+        printf("Counter: %d - Reverse Counter: %d\n",count,revcount);
+        mask = 1;
+        for(i=0; i<count; i++)
+            mask = mask << 1; // Mask will be a number that changes from 0000 0001, 0000 0010, 0000 0100, ..., 1000 0000
+        v[revcount] = ((m->attr.len & mask) ^ (m->attr.seq & mask) ^ (m->attr.type & mask)) // Here we got the xor from attrs.
+        for(i=0; i < m->attr.len; i++) // Finishes the parity of the revcount bit by looking every data byte.
+            v[revcount] = v[revcount & mask] ^ (m->data[i] & mask);
+        // Parity is a bit or a byte? I mean, is it the horizontal parity with all the 8 bits I got?
+        // Or is it all the 8 bits? I think its all the 8 bits, so, I will implement that way.
+        // Anyway, we should still check it!
+        res += v[revcount] * pow(2,count); // This should (not tested) move the bit to the <<
+                                           // how many times it has to be moved.
+    }
+    return res;
+}
+
 void error(const char *msg) {
     puts(msg);
     perror(msg);
@@ -60,7 +86,8 @@ Message prepare_msg(Attr attr, unsigned char *data) {
     if((m->data = malloc(sizeof(char) * (attr.len + 1))) == NULL)
         error("Unable to allocate memory.");
     strcpy(m->data, data);  // This was throwing an unknown error. Any ideas why?
-    m->par = 0;
+    //m->par = 0;
+    m->par = get_parity(m);
     print_message(m);
     return *m;
 }
